13ef9e2b7632d8272c090d990ef1cb65c44fc8a4
SHALINI ANGURAJ <arshalini298@gmail.com>
2024-01-18 19:25:45 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -11,12 +11,10 @@ import { Toaster } from "react-hot-toast";
 import Header from "./components/Home/Header/Header";
 import Loginpage from "./components/pages/Loginpage";
 import Registerpage from "./components/pages/Registerpage";
-//import Homepage from "./components/pages/Homepage";
 import BlogDetails from "./components/pages/BlogDetails";
 import Blogs from "./components/pages/Blogs";
 import CreateBlog from "./components/pages/CreateBlog";
 import UserBlogs from "./components/pages/UserBlogs";
-import Homepage from "./components/pages/Homepage";
 
 const Layout = () => {
   return (
@@ -34,8 +32,6 @@ const router = createBrowserRouter(
       <Route path="/" element={<Layout />}>
         <Route index element={<Blogs />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/tabs" element={<Homepage/>} />
-
         <Route path="/my-blogs" element={<UserBlogs/>} />
         <Route path="/create-blog" element={<CreateBlog />} />
         <Route path="/blog-details/:id" element={<BlogDetails />} />
--- src/components/pages/Registerpage.js
+++ src/components/pages/Registerpage.js
@@ -23,7 +23,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("/api/v1/user/register", {
+      const { data } = await axios.post("https://blog-ok12.onrender.com/api/v1/user/register", {
         username: inputs.name,
         email: inputs.email,
         password: inputs.password,
